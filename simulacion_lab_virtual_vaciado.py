35c99e876f6968ab802b00aa603b46a06683fb0f
DiyaraG <128650819+DiyaraG@users.noreply.github.com>
2026-03-29 12:45:35 -0400

    Update simulacion_lab_virtual_vaciado.py

--- simulacion_lab_virtual_vaciado.py
+++ simulacion_lab_virtual_vaciado.py
@@ -77,7 +77,7 @@ with col_l1: render_logo_institucional("logo_ucv.png", "UCV")
 with col_tit:
     st.markdown("<h1>Unidad de Simulación: Operaciones Unitarias</h1>", unsafe_allow_html=True)
     st.markdown("<p style='text-align: center; color: #5d6d7e;'>Escuela de Ingeniería Química | Facultad de Ingeniería - UCV</p>", unsafe_allow_html=True)
-with col_l2: render_logo_institucional("logo_quimica.png", "EIQ",  width=180)
+with col_l2: render_logo_institucional("logo_quimica.png", "EIQ")
 
 st.markdown("---")
 
